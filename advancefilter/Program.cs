71955e8789bfd2c81cec94a048c9719ec78cd18b
senjuily09 <gvsharshith14@gmail.com>
2026-06-25 11:42:39 +0530

    Enable Swagger UI in all environments
    Previously, Swagger and Swagger UI were only enabled in the development environment. This change removes the environment check, making Swagger and Swagger UI available regardless of the environment.

--- advancefilter/Program.cs
+++ advancefilter/Program.cs
@@ -48,11 +48,8 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
 
 
 app.UseCors("AllowFrontend");
